cc7b9dfec6f4f312d998db20eb3859449c39aa8c
Malgrin <ctapuk_87@mail.ru>
2021-07-24 18:54:07 +0300

    Работает пока только таймер без анимации и шторки

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -1,17 +1,75 @@
 package com.example.stopwatch
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.stopwatch.adapter.Stopwatch
+import com.example.stopwatch.adapter.StopwatchAdapter
+import com.example.stopwatch.adapter.StopwatchListener
 import com.example.stopwatch.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), StopwatchListener {
 
     private lateinit var binding: ActivityMainBinding
+
+    private val stopwatchAdapter = StopwatchAdapter(this)
+    private val stopwatches = mutableListOf<Stopwatch>()
+    private var nextId = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.recycler.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = stopwatchAdapter
+        }
+
+        binding.addNewStopwatchButton.setOnClickListener {
+            try {
+                val minutes = binding.setMinutesStopwatchText.text.toString().toLong() * 1000 * 60
+                if (minutes in 1..359999999) {
+                    stopwatches.add(Stopwatch(nextId++, minutes, false))
+                    stopwatchAdapter.submitList(stopwatches.toList())
+                }
+            } catch (e: NumberFormatException) {
+
+            }
+        }
+    }
+
+    override fun start(id: Int) {
+        changeStopwatch(id, null, true)
+
+    }
+
+    override fun stop(id: Int, currentMs: Long) {
+        changeStopwatch(id, currentMs, false)
+    }
+
+    override fun reset(id: Int) {
+        changeStopwatch(id, 0L, false)
+    }
+
+    override fun delete(id: Int) {
+        stopwatches.remove(stopwatches.find { it.id == id })
+        stopwatchAdapter.submitList(stopwatches.toList())
+    }
+
+    private fun changeStopwatch(id: Int, currentMs: Long?, isStarted: Boolean) {
+        val newTimers = mutableListOf<Stopwatch>()
+        stopwatches.forEach {
+            if (it.id == id) {
+                newTimers.add(Stopwatch(it.id, currentMs ?: it.currentMs, isStarted))
+            } else {
+                newTimers.add(it)
+            }
+        }
+        stopwatchAdapter.submitList(newTimers)
+        stopwatches.clear()
+        stopwatches.addAll(newTimers)
     }
 }
